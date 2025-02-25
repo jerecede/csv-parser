6293329393536bfb20e591ea4314810551f00fec
jerecede <jeremias10458@gmail.com>
2025-02-25 13:02:14 +0100

    big update

--- main.js
+++ main.js
@@ -2,24 +2,26 @@ const fs = require('fs');
 
 function readCsvFromFile(filePath) {
     try {
-        const data = fs.readFileSync(filePath, 'utf8');
-        return data;
+        const csvData = fs.readFileSync(filePath, 'utf8');
+        return csvData;
     } catch (err) {
         console.error(`Error reading file from disk: ${err}`);
     }
 }
 
-function fromCsvToJson(csv){
+function fromCsvToJson(csvData){
     // name,surname,yob,gender
     // lorenzo,puppo,1995,m
     // hugo,martinez,1994,m
     // sara,de prà,1989,f
-    const arrayOfStringRows = splitCsvInRows(csv);
+
+    const arrayOfStringRows = splitCsvInRows(csvData);
     console.log(arrayOfStringRows);
     // ["name,surname,yob,gender"]
     // ["lorenzo,puppo,1995,m"]
     // ["hugo,martinez,1994,m"]
     // ["sara,de prà,1989,f"]
+
     const arrayOfSplittedRows = splitRows(arrayOfStringRows);
     console.log(arrayOfSplittedRows);
     // [
@@ -28,43 +30,98 @@ function fromCsvToJson(csv){
     // ["hugo","martinez",1994,"m"]
     // ["sara","de prà",1989,"f"]
     // ]
-    // const keys = getKeysFromFirstLine(arrayOfSplittedRows);
+
+    const keys = getKeysFromFirstLine(arrayOfSplittedRows);
+    console.log(keys);
     // ["name","surname","yob","gender"]
-    // const values = getValue(arrayOfSplittedRows);
+
+    const values = getValues(arrayOfSplittedRows);
+    console.log(values);
     // [
     // ["lorenzo","puppo",1995,"m"]
     // ["hugo","martinez",1994,"m"]
     // ["sara","de prà",1989,"f"]
     // ]
-    // const ObjectFromEntries = createObjectOfEntries(keys,values);
-    // const json = convertObjectToJson(ObjectFromEntries);
-    // return json;
+
+    const ObjectFromEntries = createObjectOfEntries(keys,values);
+    console.log(ObjectFromEntries);
+    
+    const json = convertObjectToJson(ObjectFromEntries);
+    return json;
 }
 
-function splitCsvInRows(data) {
-    const arrayDataRows = data.split(/\r?\n/);
-    return arrayDataRows;
+function splitCsvInRows(csvData) {
+    const arrayData = csvData.split(/\r?\n/);
+    return arrayData;
 }
 
 function splitRows(arrayData){
-    let newArray = [];
+    let newArrayData = [];
 
     for (let i = 0; i < arrayData.length; i++) {
         const stringRow = arrayData[i];
 
         const arrayStringRow = stringRow.split(',');
-        newArray.push(arrayStringRow);
+        newArrayData.push(arrayStringRow);
     }
 
-    return newArray;
+    return newArrayData;
+}
+
+function getKeysFromFirstLine(arrayData){
+    const arrayKeys = [...arrayData[0]];
+    return arrayKeys;
+}
+
+function getValues(arrayData){
+    let arrayOfValues = [];
+    for (let i = 1; i < arrayData.length; i++) {
+        const array = arrayData[i];
+        arrayOfValues.push(array);
+    }
+    //oppure arrayOfValues = arrayData.slice(1);
+
+    return arrayOfValues;
+}
+
+function createObjectOfEntries(keys, values){
+    const arrayOfEntries = [];
+
+    for (let i = 0; i < values.length; i++) {
+        const valueArray = values[i];
+        
+        const entry = createEntry(keys, valueArray)
+
+        arrayOfEntries.push(entry);
+    }
+
+    return arrayOfEntries;
+}
+
+function createEntry(keys, valueArray){
+    const obj = {};
+
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        const value = valueArray[i];
+        obj[key] = value;
+    }
+
+    return obj;
+}
+
+function convertObjectToJson(object) {
+    return JSON.stringify(object);
 }
 
 function main() {
 
-    const csvData = readCsvFromFile("./data/test1.csv"); //leggo
-    console.log(csvData);
+    const csv = readCsvFromFile("./data/test1.csv"); //leggo
+    console.log(csv);
 
-    const json = fromCsvToJson(csvData); //trasformo
+    const json = fromCsvToJson(csv); //trasformo
+    console.log(json);
+    
 
     // writeJsonToFile(filePath); //scrivo
 }
